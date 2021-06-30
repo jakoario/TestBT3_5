3bdeebe5d9f1d09971affa4c4fcb6751e8bca91d
jakoario <j.andre@koario.fr>
2021-06-30 10:22:46 +0200

    C2

--- app/src/main/java/com/example/testbt3_5/BluetoothActivity.kt
+++ app/src/main/java/com/example/testbt3_5/BluetoothActivity.kt
@@ -1,24 +1,29 @@
 package com.example.testbt3_5
 
 
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.*
 import android.text.method.ScrollingMovementMethod
 import android.view.WindowManager
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.util.*
 
 
-class BluetoothActivity : AppCompatActivity() {
+class BluetoothActivity : AppCompatActivity(), LocationListener {
 
     private lateinit var textviewData: TextView
     private lateinit var speedData: TextView
     private lateinit var distanceData: TextView
     private lateinit var address: String
+    private lateinit var speedGPSData: TextView
     var distanceCumul: Double = 0.0
 
 
@@ -35,9 +40,22 @@ class BluetoothActivity : AppCompatActivity() {
         speedData = findViewById(R.id.speed_text)
         speedData.movementMethod = ScrollingMovementMethod() // make it scrollable
         distanceData = findViewById(R.id.dist_text)
+        speedGPSData = findViewById(R.id.tv_speedGPS)
+
+        // check for gps permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1000)
+        } else {
+            // start the program if the permission is granted
+            doStuff()
+        }
 
         val bt = BtInterface(handler, address)
         bt.connect()
+
+        this.updateSpeed(null)
 
     }
 
@@ -71,6 +89,72 @@ class BluetoothActivity : AppCompatActivity() {
                     val rounddist = BigDecimal(distanceCumul).setScale(2, RoundingMode.HALF_EVEN) // arrondi à 2 décimales
                     distanceData.text = rounddist.toString() + " m"
                 }
+            }
+        }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        if (location != null) run {
+            val myLocation = CLocation(location)
+            this.updateSpeed(myLocation)
+        }
+    }
+
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+
+    }
+
+    private fun doStuff() {
+        val locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
+        if(locationManager !=null) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0.0F, this)
+        }
+        Toast.makeText(this, "Waiting for GPS connection", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updateSpeed(location: CLocation?) {
+        var nCurrentSpeed: Float = 0F
+
+        if (location != null) {
+            nCurrentSpeed = location.speed
+        }
+        val fmt = Formatter(StringBuilder())
+        fmt.format(Locale.FRANCE, "%5.1f", nCurrentSpeed)
+        var strCurrentSpeed: String = fmt.toString()
+        strCurrentSpeed = strCurrentSpeed.replace(" ", "0")
+
+        speedGPSData.text = strCurrentSpeed + " km/h"
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                doStuff()
+            } else {
+                finish()
             }
         }
     }
