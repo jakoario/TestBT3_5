799a415674657dcac5c3f8e53f4684473964378f
jakoario <j.andre@koario.fr>
2021-07-02 14:41:05 +0200

    C3

--- app/src/main/java/com/example/testbt3_5/BluetoothActivity.kt
+++ app/src/main/java/com/example/testbt3_5/BluetoothActivity.kt
@@ -22,7 +22,6 @@ import kotlin.math.round
 
 class BluetoothActivity : AppCompatActivity(), LocationListener {
 
-    private lateinit var textviewData: TextView
     private lateinit var speedData: TextView
     private lateinit var distanceData: TextView
     private lateinit var address: String
@@ -40,10 +39,7 @@ class BluetoothActivity : AppCompatActivity(), LocationListener {
 
         address = intent.getStringExtra(MainActivity.EXTRA_ADDRESS).toString()
 
-        textviewData = findViewById(R.id.receive_text)
-        textviewData.movementMethod = ScrollingMovementMethod() // make it scrollable
         speedData = findViewById(R.id.speed_text)
-        speedData.movementMethod = ScrollingMovementMethod() // make it scrollable
         distanceData = findViewById(R.id.dist_text)
         speedGPSData = findViewById(R.id.tv_speedGPS)
         distanceGPSData = findViewById(R.id.dist_textGPS)
@@ -74,7 +70,6 @@ class BluetoothActivity : AppCompatActivity(), LocationListener {
         override fun handleMessage(msg: Message) {
             val data = msg.data.getString("receivedData")
             if (data != null) {
-                textviewData.append(data)
 
                 // Calcul et envoie de la vitesse en km/h
                 val speed: Double? = data.toDoubleOrNull()
@@ -83,12 +78,12 @@ class BluetoothActivity : AppCompatActivity(), LocationListener {
                     val calculspeed: Double = speed * 1.44 / periode
                     if (calculspeed != 0.0) {
                         val roundspeed = BigDecimal(calculspeed).setScale(2, RoundingMode.HALF_EVEN) // arrondi à 2 décimales
-                        speedData.text = roundspeed.toString() + "\n"
+                        speedData.text = roundspeed.toString()
 
                         // sauvegarde de donnees
                         if (isExternalStorageReadable()) {
-                            saveData("saveOBDspeedData1.txt", speedData.text.toString())
-                            saveData("saveGPSspeedData1.txt", speedGPSData.text.toString())
+                            saveData("saveOBDspeedData1.txt", speedData.text.toString() + "\n")
+                            saveData("saveGPSspeedData1.txt", speedGPSData.text.toString() + "\n")
                         }
                     }
                 } else {
@@ -100,8 +95,6 @@ class BluetoothActivity : AppCompatActivity(), LocationListener {
                     distanceCumul += speed * 0.4
                     val rounddist = BigDecimal(distanceCumul).setScale(2, RoundingMode.HALF_EVEN) // arrondi à 2 décimales
                     distanceData.text = rounddist.toString() + " m"
-
-
                 }
             }
         }
@@ -157,7 +150,7 @@ class BluetoothActivity : AppCompatActivity(), LocationListener {
         var strCurrentSpeed: String = fmt.toString()
         strCurrentSpeed = strCurrentSpeed.replace(" ", "0")
 
-        speedGPSData.text = strCurrentSpeed + " km/h \n"
+        speedGPSData.text = strCurrentSpeed
 
         // distance cumulee
         if (::oldLocation.isInitialized) {  // oldLocation doit être initialisé pour pouvoir l'utiliser
